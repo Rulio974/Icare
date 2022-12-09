3344e613ea230bf9d6d4e856918f4a44c8efedfe
rulio974 <57720375+Rulio974@users.noreply.github.com>
2022-12-09 21:32:49 +0100

    Update monitor_mode.sh

--- monitor_mode.sh
+++ monitor_mode.sh
@@ -1,14 +1,83 @@
 #!/bin/bash
 
-
-if [[ $# != 1 ]]
-then
-	echo "erreur arguments"
-	exit 0
-fi
+I=""
 
 
-sudo ifconfig $1 down
-sudo iwconfig $1 mode monitor
-sudo ifconfig $1 up
+function auto() {
+
+
+    iw dev | grep -Po '^\sInterface\s\K.*$' | cat -n > tmp.txt
+    cat tmp.txt
+    word_count=$(wc -l tmp.txt | cut -d " " -f 1)
+
+
+    echo -e "\033[1;31m\nchoisir l'interface : \c"
+
+    read interface
+
+    if [[ $interface -gt $word_count ]]
+    then
+        echo "erreur choix interface"
+        rm tmp.txt
+        exit 0
+    fi
+
+    sed_interface="${interface}p"
+
+    I=$(iw dev | grep -Po '^\sInterface\s\K.*$' | sed -n $sed_interface)
+
+    rm tmp.txt
+
+
+}
+
+function run() {
+
+    sudo ifconfig $I down
+    sudo iwconfig $I mode monitor
+    sudo ifconfig $I up
+
+    exit 0
+
+}
+
+
+
+function help() {
+    echo "usage : ./monitor_mode.sh <option> <interface>"
+    echo "-a : list interfaces"
+    echo "-m : manual"
+
+    exit 0
+
+}
+
+
+
+while getopts "ham:" option; do
+    case "${option}" in
+        a)
+            auto
+            run
+            ;;
+        m)
+            I=${OPTARG}
+            run
+            ;;
+        ?)
+            help
+            exit 0
+            ;;
+        h)
+            help
+            exit 0
+            ;;
+        
+        
+    esac
+done
+
+help
+
+
 
